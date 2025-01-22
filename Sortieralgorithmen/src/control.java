c773a8b84f0d3d7c1b9ad2e6d3ba6e6ed6b083e3
Vivecith <jonas485@schule.bremen.de>
2025-01-22 10:48:53 +0100

    new

--- Sortieralgorithmen/src/control.java
+++ Sortieralgorithmen/src/control.java
@@ -5,30 +5,73 @@ import java.util.Arrays;
 public class control {
     public static void main(String[] args) throws IOException {
         MenuText(0);
-        MenuText(1);
-        boolean repeat1 = false;
-        {
-            boolean repeat2 = false;
-            System.out.print("Ihre Eingabe");
-            String input = IO.readString();
+        boolean repeat1;
+        do {
+            repeat1 = false;
+            boolean repeat2;
+            ISortierbar selection = null;
+            do {
+                MenuText(1);
+                repeat2 = false;
+                System.out.print("\nIhre Eingabe: ");
+                String input = IO.readString();
+                switch (input) {
+                    case "0": {
+                        selection = new Selectionsort();
+                        break;
+                    }
+                    case "1": {
+                        selection = new Bubblesort();
+                        break;
+                    }
+                    case "2": {
+                        selection = new Insertionsort();
+                        break;
+                    }
+                    case "3": {
+                        selection = new Quicksort();
+                        break;
+                    }
+                    case "x": {
+                        System.out.println("Programm wird geschloßen...");
+                        System.exit(100);
+                    }
+                    default: {
+                        System.out.println("Ungültige Eingabe!");
+                        repeat2 = true;
+                    }
+                }
+            } while (repeat2);
+            do {
+                MenuText(2);
+                System.out.print("\nIhre Eingabe: ");
+                String input = IO.readString();
+                if (!input.equals("x")) {
+                    try {
+                        double[] list = new double[input.split("[;,]").length];
+                        for (int i = 0; i < list.length; i++) {
+                            list[i] = Double.parseDouble(input.split("[;,]")[i]);
+                        }
+                        //{17.07, 14.83, 3.65, 1.36, 16.23, 13.83, 18.55, 15.42, 8.72, 3.06, 13.53, 16.3, 13.73, 5.46, 11.85, 2.99, 12.13, 10.44, 7.25, 15.58};
+                        ArrayList<Double> werte = new ArrayList<>();
+                        for (double d : list) {
+                            werte.add(d);
+                        }
+                        System.out.println(selection.sortiereASC(werte));
+                        System.out.println(selection.sortiereDESC(werte));
 
-            Selectionsort selection1 = new Selectionsort();
-
-            Bubblesort selection2 = new Bubblesort();
-
-            Insertionsort selection3 = new Insertionsort();
-
-            Quicksort selection4 = new Quicksort();
-
-            double[] list = {17.07, 14.83, 3.65, 1.36, 16.23, 13.83, 18.55, 15.42, 8.72, 3.06, 13.53, 16.3, 13.73, 5.46, 11.85, 2.99, 12.13, 10.44, 7.25, 15.58};
-            ArrayList<Double> werte = new ArrayList<>();
-            for (double d : list) {
-                werte.add(d);
-            }
-            System.out.println(selection1.sortiereASC(werte));
-            System.out.println(selection1.sortiereDESC(werte));
-
-            System.out.print("Wollen Sie noch eine Sortierung ausführen?");
+                        System.out.print("\nWollen Sie noch eine Sortierung ausführen [y/n]?: ");
+                        repeat1 = IO.readBoolean();
+                        repeat2 = false;
+                    } catch (Exception e) {
+                        System.out.println("Fehler beim Ausführen der Sortierung!");
+                        repeat2 = true;
+                    }
+                } else {
+                    repeat1 = true;
+                    repeat2 = false;
+                }
+            } while (repeat2);
         }while(repeat1);
         /*
         TEST
@@ -66,11 +109,11 @@ public class control {
     public static void MenuText(int index) { //Text-Methode ohne variable Ausgaben
         switch (index) {
             case 0 -> //name text
-                System.out.println("""
-                        Algorithmus-Programm
-                        
-                        Willkommen Nutzer!
-                        """);
+                    System.out.println("""
+                            Algorithmus-Programm
+                            
+                            Willkommen Nutzer!
+                            """);
             case 1 -> //menu text
                     System.out.println("""
                             Bitte wählen Sie, welchen Sortieralgorithmus Sie verwenden möchten.
@@ -83,14 +126,10 @@ public class control {
                             """);
             case 2 -> //number insert prompt
                     System.out.println("""
-                            Bitte geben Sie die Liste an, die Sie Sortieren möchten.
+                            Bitte geben Sie die Listenwerte an, die Sie Sortieren möchten.
+                            Die Werte sollten in einer Reihe und mit ';' oder ',' getrennt sein.
                             Oder 'x', wenn Sie zurück zum Menu möchten.
                             """);
-        }
-    }
-    public static void MenuText(int index, String text) { //Text-Methode mit variablen Ausgaben
-        switch (index) {
-
         }
     }
 }
