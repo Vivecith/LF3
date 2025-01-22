011401968acfe23965ef8393afa287e8544138da
Vivecith <jonas485@schule.bremen.de>
2025-01-22 08:56:55 +0100

    new

---
+++ Arbeit/src/SortierAlgorithmen/Quicksort.java
@@ -0,0 +1,61 @@
+package SortierAlgorithmen;
+
+import java.util.ArrayList;
+
+public class Quicksort implements ISortierbar{
+    @Override
+    public ArrayList<Double> sortiereASC(ArrayList<Double> werte) {
+        ArrayList<Double> werte2 = new ArrayList<>(werte);
+        ArrayList<Double> sortiert = new ArrayList<>();
+
+        if(werte2.size() > 1) {
+            ArrayList<Double> temp1 = new ArrayList<>();
+            ArrayList<Double> temp2 = new ArrayList<>();
+            double p = werte2.get(0);
+            for (int i = 1; i < werte2.size(); i++) {
+                if (p > werte2.get(i)) {
+                    temp1.add(werte2.get(i));
+                } else if (p < werte2.get(i)) {
+                    temp2.add(werte2.get(i));
+                }
+            }
+            temp1 = sortiereASC(temp1);
+            temp2 = sortiereASC(temp2);
+
+            sortiert.addAll(temp1);
+            sortiert.add(p);
+            sortiert.addAll(temp2);
+        } else {
+            sortiert = werte;
+        }
+        return sortiert;
+    }
+
+    @Override
+    public ArrayList<Double> sortiereDESC(ArrayList<Double> werte) {
+        ArrayList<Double> werte2 = new ArrayList<>(werte);
+        ArrayList<Double> sortiert = new ArrayList<>();
+
+        if(werte2.size() > 1) {
+            ArrayList<Double> temp1 = new ArrayList<>();
+            ArrayList<Double> temp2 = new ArrayList<>();
+            double p = werte2.get(0);
+            for (int i = 1; i < werte2.size(); i++) {
+                if (p < werte2.get(i)) {
+                    temp1.add(werte2.get(i));
+                } else if (p > werte2.get(i)) {
+                    temp2.add(werte2.get(i));
+                }
+            }
+            temp1 = sortiereDESC(temp1);
+            temp2 = sortiereDESC(temp2);
+
+            sortiert.addAll(temp1);
+            sortiert.add(p);
+            sortiert.addAll(temp2);
+        } else {
+            sortiert = werte;
+        }
+        return sortiert;
+    }
+}
--- Arbeit/src/SortierAlgorithmen/control.java
+++ Arbeit/src/SortierAlgorithmen/control.java
@@ -5,16 +5,46 @@ import java.util.Arrays;
 
 public class control {
     public static void main(String[] args) {
-        double[] list = {2.3, 3.5, 5.8, 2.2, 1.2, 4.5, 5.6, 3.4, 6.7, 7.8};
+        double[] list = {17.07,14.83,3.65,1.36,16.23,13.83,18.55,15.42,8.72,3.06,13.53,16.3,13.73,5.46,11.85,2.99,12.13,10.44,7.25,15.58};
         ArrayList<Double> werte = new ArrayList<>();
         for (double d : list) {
             werte.add(d);
         }
+        Selectionsort selection1 = new Selectionsort();
+        Bubblesort selection2 = new Bubblesort();
+        Insertionsort selection3 = new Insertionsort();
+        Quicksort selection4 = new Quicksort();
+        System.out.println(selection1.sortiereASC(werte));
+        System.out.println(selection1.sortiereDESC(werte));
 
-        Insertionsort selection = new Insertionsort();
-        System.out.println(selection.sortiereASC(werte));
-        System.out.println(selection.sortiereDESC(werte));
+        /*
+        SortASC:
+            Selectionsort:
+            [1.36, 2.99, 3.06, 3.65, 5.46, 7.25, 8.72, 10.44, 11.85, 12.13, 13.53, 13.73, 13.83, 14.83, 15.42, 15.58, 16.23, 16.3, 17.07, 18.55]
 
+            Bubblesort:
+            [1.36, 2.99, 3.06, 3.65, 5.46, 7.25, 8.72, 10.44, 11.85, 12.13, 13.53, 13.73, 13.83, 14.83, 15.42, 15.58, 16.23, 16.3, 17.07, 18.55]
+
+            Insertionsort:
+            [1.36, 2.99, 3.06, 3.65, 5.46, 7.25, 8.72, 10.44, 11.85, 12.13, 13.53, 13.73, 13.83, 14.83, 15.42, 15.58, 16.23, 16.3, 17.07, 18.55]
+
+            Quicksort:
+            [1.36, 2.99, 3.06, 3.65, 5.46, 7.25, 8.72, 10.44, 11.85, 12.13, 13.53, 13.73, 13.83, 14.83, 15.42, 15.58, 16.23, 16.3, 17.07, 18.55]
+
+        SortDESC:
+            Selectionsort:
+            [18.55, 17.07, 16.3, 16.23, 15.58, 15.42, 14.83, 13.83, 13.73, 13.53, 12.13, 11.85, 10.44, 8.72, 7.25, 5.46, 3.65, 3.06, 2.99, 1.36]
+
+            Bubblesort:
+            [18.55, 17.07, 16.3, 16.23, 15.58, 15.42, 14.83, 13.83, 13.73, 13.53, 12.13, 11.85, 10.44, 8.72, 7.25, 5.46, 3.65, 3.06, 2.99, 1.36]
+
+            Insertionsort:
+            [18.55, 17.07, 16.3, 16.23, 15.58, 15.42, 14.83, 13.83, 13.73, 13.53, 12.13, 11.85, 10.44, 8.72, 7.25, 5.46, 3.65, 3.06, 2.99, 1.36]
+
+            Quicksort:
+            [18.55, 17.07, 16.3, 16.23, 15.58, 15.42, 14.83, 13.83, 13.73, 13.53, 12.13, 11.85, 10.44, 8.72, 7.25, 5.46, 3.65, 3.06, 2.99, 1.36]
+
+         */
     }
 
 }
