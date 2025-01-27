3832b9c40711d889d1a3e18602c771b6d6f9103a
Vivecith <jonas485@schule.bremen.de>
2025-01-27 13:04:12 +0100

    neu

--- Feuerwehr/src/Main.java
+++ Feuerwehr/src/Main.java
@@ -1,11 +1,14 @@
 public class Main {
     public static void main(String[] args) {
         Brandmeister brandmeister = new Brandmeister();
+
         Feuerwehrmann feuerwehrmann1 = new Feuerwehrmann("Hans","Günter");
         Feuerwehrmann feuerwehrmann2 = new Feuerwehrmann("Jason", "Momoa");
+        Feuerwehrmann feuerwehrmann3 = new Feuerwehrmann("Johnathan", "Smith");
 
         brandmeister.addObserver(feuerwehrmann1);
         brandmeister.addObserver(feuerwehrmann2);
+        brandmeister.addObserver(feuerwehrmann3);
 
         brandmeister.work();
     }
