3bd8b8e36831d532279c6c01d78b2999275a3688
Vivecith <jonas485@schule.bremen.de>
2025-01-29 10:35:19 +0100

    neu

--- oop/src/TestIO.java
+++ oop/src/TestIO.java
@@ -4,5 +4,7 @@ public class TestIO {
 // this class is a test for IO.java and therefore requires IO.java to be in the same folder.
     public static void main(String[] args) throws IOException {
         System.out.println(IO.Colours.Templates.FALLOUTNV("HALLO WELT!"));
+        String text = IO.readString();
+        System.out.println(IO.Colours.Templates.FALLOUTNVINVERSE(text));
     }
 }
