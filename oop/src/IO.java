79e74834c02c86eb7b7dba6b80b7f12304b0d081
Vivecith <jonas485@schule.bremen.de>
2025-01-29 09:40:44 +0100

    neu

--- oop/src/IO.java
+++ oop/src/IO.java
@@ -119,11 +119,16 @@ public class IO {
         public static final String GREEN = "\u001B[32m";
         public static final String YELLOW = "\u001B[33m";
         public static final String BLUE = "\u001B[34m";
-        public static final String PURPLE = "\u001B[35m";
+        public static final String MAGENTA = "\u001B[35m";
         public static final String CYAN = "\u001B[36m";
         public static final String WHITE = "\u001B[37m";
 
         public static final String ORANGE = "\u001B[38;5;166m";
+        public static final String CRIMSON = "\u001B[38;5;88m";
+        public static final String NAVY = "\u001B[38;5;18m";
+        public static final String VIOLET = "\u001B[38;5;55m";
+        public static final String SCARLET = "\u001B[38;5;160m";
+        public static final String DARKBLUE = "\u001B[38;5;20m";
 
         //BOLD
         public static final String BLACK_BOLD = "\u001B[1;30m";
@@ -131,11 +136,15 @@ public class IO {
         public static final String GREEN_BOLD = "\u001B[1;32m";
         public static final String YELLOW_BOLD = "\u001B[1;33m";
         public static final String BLUE_BOLD = "\u001B[1;34m";
-        public static final String PURPLE_BOLD = "\u001B[1;35m";
+        public static final String MAGENTA_BOLD = "\u001B[1;35m";
         public static final String CYAN_BOLD = "\u001B[1;36m";
         public static final String WHITE_BOLD = "\u001B[1;37m";
 
         public static final String ORANGE_BOLD = "\u001B[1;38;5;166m";
+        public static final String CRIMSON_BOLD = "\u001B[1;38;5;88m";
+        public static final String NAVY_BOLD = "\u001B[1;38;5;18m";
+        public static final String VIOLET_BOLD = "\u001B[1;38;5;55m";
+        public static final String SCARLET_BOLD = "\u001B[1;38;5;160m";
 
         //ITALIC
         public static final String BLACK_ITALIC = "\u001B[3;30m";
@@ -143,11 +152,15 @@ public class IO {
         public static final String GREEN_ITALIC = "\u001B[3;32m";
         public static final String YELLOW_ITALIC = "\u001B[3;33m";
         public static final String BLUE_ITALIC = "\u001B[3;34m";
-        public static final String PURPLE_ITALIC = "\u001B[3;35m";
+        public static final String MAGENTA_ITALIC = "\u001B[3;35m";
         public static final String CYAN_ITALIC = "\u001B[3;36m";
         public static final String WHITE_ITALIC = "\u001B[3;37m";
 
         public static final String ORANGE_ITALIC = "\u001B[3;38;5;166m";
+        public static final String CRIMSON_ITALIC = "\u001B[3;38;5;88m";
+        public static final String NAVY_ITALIC = "\u001B[3;38;5;18m";
+        public static final String VIOLET_ITALIC = "\u001B[3;38;5;55m";
+        public static final String SCARLET_ITALIC = "\u001B[3;38;5;160m";
 
         //UNDERLINED
         public static final String BLACK_LINED = "\u001B[4;30m";
@@ -155,11 +168,15 @@ public class IO {
         public static final String GREEN_LINED = "\u001B[4;32m";
         public static final String YELLOW_LINED = "\u001B[4;33m";
         public static final String BLUE_LINED = "\u001B[4;34m";
-        public static final String PURPLE_LINED = "\u001B[4;35m";
+        public static final String MAGENTA_LINED = "\u001B[4;35m";
         public static final String CYAN_LINED = "\u001B[4;36m";
         public static final String WHITE_LINED = "\u001B[4;37m";
 
-        public static final String ORANGE_LINED = "\u001B[38;5;166m";
+        public static final String ORANGE_LINED = "\u001B[4;38;5;166m";
+        public static final String CRIMSON_LINED = "\u001B[4;38;5;88m";
+        public static final String NAVY_LINED = "\u001B[4;38;5;18m";
+        public static final String VIOLET_LINED = "\u001B[4;38;5;55m";
+        public static final String SCARLET_LINED = "\u001B[4;38;5;160m";
 
         //BRIGHT
         public static final String BLACK_BRIGHT = "\u001B[90m";
@@ -167,11 +184,15 @@ public class IO {
         public static final String GREEN_BRIGHT = "\u001B[92m";
         public static final String YELLOW_BRIGHT = "\u001B[93m";
         public static final String BLUE_BRIGHT = "\u001B[94m";
-        public static final String PURPLE_BRIGHT = "\u001B[95m";
+        public static final String MAGENTA_BRIGHT = "\u001B[95m";
         public static final String CYAN_BRIGHT = "\u001B[96m";
         public static final String WHITE_BRIGHT = "\u001B[97m";
 
         public static final String ORANGE_BRIGHT = "\u001B[38;5;202m";
+        public static final String CRIMSON_BRIGHT = "\u001B[38;5;124m";
+        public static final String NAVY_BRIGHT = "\u001B[38;5;19m";
+        public static final String VIOLET_BRIGHT = "\u001B[38;5;56m";
+        public static final String SCARLET_BRIGHT = "\u001B[38;5;196m";
 
         //BOLD + BRIGHT
         public static final String BLACK_BOLD_BRIGHT = "\u001B[1;90m";
@@ -179,11 +200,15 @@ public class IO {
         public static final String GREEN_BOLD_BRIGHT = "\u001B[1;92m";
         public static final String YELLOW_BOLD_BRIGHT = "\u001B[1;93m";
         public static final String BLUE_BOLD_BRIGHT = "\u001B[1;94m";
-        public static final String PURPLE_BOLD_BRIGHT = "\u001B[1;95m";
+        public static final String MAGENTA_BOLD_BRIGHT = "\u001B[1;95m";
         public static final String CYAN_BOLD_BRIGHT = "\u001B[1;96m";
         public static final String WHITE_BOLD_BRIGHT = "\u001B[1;97m";
 
         public static final String ORANGE_BOLD_BRIGHT = "\u001B[1;38;5;202m";
+        public static final String CRIMSON_BOLD_BRIGHT = "\u001B[1;38;5;124m";
+        public static final String NAVY_BOLD_BRIGHT = "\u001B[1;38;5;19m";
+        public static final String VIOLET_BOLD_BRIGHT = "\u001B[1;38;5;56m";
+        public static final String SCARLET_BOLD_BRIGHT = "\u001B[1;38;5;196m";
 
         //ITALIC + BRIGHT
         public static final String BLACK_ITALIC_BRIGHT = "\u001B[3;90m";
@@ -191,11 +216,15 @@ public class IO {
         public static final String GREEN_ITALIC_BRIGHT = "\u001B[3;92m";
         public static final String YELLOW_ITALIC_BRIGHT = "\u001B[3;93m";
         public static final String BLUE_ITALIC_BRIGHT = "\u001B[3;94m";
-        public static final String PURPLE_ITALIC_BRIGHT = "\u001B[3;95m";
+        public static final String MAGENTA_ITALIC_BRIGHT = "\u001B[3;95m";
         public static final String CYAN_ITALIC_BRIGHT = "\u001B[3;96m";
         public static final String WHITE_ITALIC_BRIGHT = "\u001B[3;97m";
 
         public static final String ORANGE_ITALIC_BRIGHT = "\u001B[3;38;5;202m";
+        public static final String CRIMSON_ITALIC_BRIGHT = "\u001B[3;38;5;124m";
+        public static final String NAVY_ITALIC_BRIGHT = "\u001B[3;38;5;19m";
+        public static final String VIOLET_ITALIC_BRIGHT = "\u001B[3;38;5;56m";
+        public static final String SCARLET_ITALIC_BRIGHT = "\u001B[3;38;5;196m";
 
         //UNDERLINED + BRIGHT
         public static final String BLACK_LINED_BRIGHT = "\u001B[4;90m";
@@ -203,11 +232,15 @@ public class IO {
         public static final String GREEN_LINED_BRIGHT = "\u001B[4;92m";
         public static final String YELLOW_LINED_BRIGHT = "\u001B[4;93m";
         public static final String BLUE_LINED_BRIGHT = "\u001B[4;94m";
-        public static final String PURPLE_LINED_BRIGHT = "\u001B[4;95m";
+        public static final String MAGENTA_LINED_BRIGHT = "\u001B[4;95m";
         public static final String CYAN_LINED_BRIGHT = "\u001B[4;96m";
         public static final String WHITE_LINED_BRIGHT = "\u001B[4;97m";
 
         public static final String ORANGE_LINED_BRIGHT = "\u001B[4;38;5;202m";
+        public static final String CRIMSON_LINED_BRIGHT = "\u001B[4;38;5;124m";
+        public static final String NAVY_LINED_BRIGHT = "\u001B[4;38;5;19m";
+        public static final String VIOLET_LINED_BRIGHT = "\u001B[4;38;5;56m";
+        public static final String SCARLET_LINED_BRIGHT = "\u001B[4;38;5;196m";
 
         //BACKGROUND
         public static final String BLACKBACK = "\u001B[40m";
@@ -215,11 +248,15 @@ public class IO {
         public static final String GREENBACK = "\u001B[42m";
         public static final String YELLOWBACK = "\u001B[43m";
         public static final String BLUEBACK = "\u001B[44m";
-        public static final String PURPLEBACK = "\u001B[45m";
+        public static final String MAGENTABACK = "\u001B[45m";
         public static final String CYANBACK = "\u001B[46m";
         public static final String WHITEBACK = "\u001B[47m";
 
         public static final String ORANGEBACK = "\u001B[48;5;166m";
+        public static final String CRIMSONBACK = "\u001B[48;5;88m";
+        public static final String NAVYBACK = "\u001B[48;5;18m";
+        public static final String VIOLETBACK = "\u001B[48;5;55m";
+        public static final String SCARLETBACK = "\u001B[48;5;160m";
 
         //BACKGROUND + HIGH BRIGHTNESS
         public static final String BLACKBACK_BRIGHT = "\u001B[100m";
@@ -227,11 +264,22 @@ public class IO {
         public static final String GREENBACK_BRIGHT = "\u001B[102m";
         public static final String YELLOWBACK_BRIGHT = "\u001B[103m";
         public static final String BLUEBACK_BRIGHT = "\u001B[104m";
-        public static final String PURPLEBACK_BRIGHT = "\u001B[105m";
+        public static final String MAGENTABACK_BRIGHT = "\u001B[105m";
         public static final String CYANBACK_BRIGHT = "\u001B[106m";
         public static final String WHITEBACK_BRIGHT = "\u001B[107m";
 
         public static final String ORANGEBACK_BRIGHT = "\u001B[48;5;202m";
+        public static final String CRIMSONBACK_BRIGHT = "\u001B[48;5;124m";
+        public static final String NAVYBACK_BRIGHT = "\u001B[48;5;19m";
+        public static final String VIOLETBACK_BRIGHT = "\u001B[48;5;56m";
+        public static final String SCARLETBACK_BRIGHT = "\u001B[48;5;196m";
+
+        public static String CUSTOM_FONT(int value) {
+            return "\u001B[38;5;" + value + "m";
+        }
+        public static String CUSTOM_BACK(int value) {
+            return "\u001B[48;5;" + value + "m";
+        }
 
         //Stylingvorlagen
         public static class Templates {
@@ -267,7 +315,7 @@ public class IO {
             }
             public static String RAINBOW(String text) {
                 StringBuilder text2 = new StringBuilder();
-                String[] colours = {REDBACK,GREENBACK,BLUEBACK};
+                String[] colours = {SCARLETBACK, ORANGEBACK, YELLOWBACK_BRIGHT, GREENBACK_BRIGHT, CYANBACK, NAVYBACK_BRIGHT, VIOLETBACK};
                 for (int i = 0; i < text.length(); i++) {
                     text2.append(colours[i % colours.length]).append(text.charAt(i));
                 }
--- oop/src/TestIO.java
+++ oop/src/TestIO.java
@@ -3,5 +3,6 @@ import java.io.IOException;
 public class TestIO {
 // this class is a test for IO.java and therefore requires IO.java to be in the same folder.
     public static void main(String[] args) throws IOException {
+        System.out.println(IO.Colours.Templates.RAINBOW("HALLO WELT, IHR BASTARDE"));
     }
 }
